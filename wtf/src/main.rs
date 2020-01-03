dc067dc81fb26b7c09a702c3148c3796db0052ad
Michael Zhang <iptq@protonmail.com>
2020-01-03 06:24:50 -0600

    aaa

--- wtf/src/main.rs
+++ wtf/src/main.rs
@@ -3,6 +3,6 @@ use std::io::Cursor;
 use wtf::Parser;
 
 fn main() {
-    let parser = Parser::new();
-    println!("{:?}", parser.parse_E("1+2*3"));
+    let mut parser = Parser::new("1+2*3");
+    println!("{:?}", parser.parse_E());
 }
